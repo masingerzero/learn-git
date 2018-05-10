8e30579a3c6ac9353e90ab372957572bc849dc7b
jfalvarez <jfalvarez@paradigmadigital.com>
2018-05-10 18:16:13 +0200

    change 2

--- src/main/java/masinger/git/test/FooBar2.java
+++ src/main/java/masinger/git/test/FooBar2.java
@@ -2,4 +2,20 @@ package masinger.git.test;
 
 public class FooBar2 {
 
+	private int bar;
+
+	public FooBar2(int bar) {
+		super();
+		this.bar = bar;
+	}
+
+	public int getBar() {
+		return bar;
+	}
+
+	public void setBar(int bar) {
+		this.bar = bar;
+	}
+	
+	
 }
