6877d7587909bb7cedb2b11320e0cf454dc6850a
jfalvarez <jfalvarez@paradigmadigital.com>
2018-05-11 10:47:37 +0200

    in develop getters to FooBar

--- src/main/java/masinger/git/test/FooBar.java
+++ src/main/java/masinger/git/test/FooBar.java
@@ -2,4 +2,20 @@ package masinger.git.test;
 
 public class FooBar {
 
+	private String foo;
+
+	public FooBar(String foo) {
+		super();
+		this.foo = foo;
+	}
+
+	public String getFoo() {
+		return foo;
+	}
+
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
+	
+	
 }
