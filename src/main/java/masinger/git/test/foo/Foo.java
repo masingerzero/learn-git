0f6702455ef5613541b2c219664faa07e0c0aeba
jfalvarez <jfalvarez@paradigmadigital.com>
2018-05-11 10:45:32 +0200

    develop branch change FooBar

--- src/main/java/masinger/git/test/foo/Foo.java
+++ src/main/java/masinger/git/test/foo/Foo.java
@@ -1,5 +1,17 @@
 package masinger.git.test.foo;
 
 public class Foo {
+	
+	private String fooBar;
 
+	public String getFooBar() {
+		return fooBar;
+	}
+
+	public void setFooBar(String fooBar) {
+		this.fooBar = fooBar;
+	}
+	
+	
+	
 }
